51f40d2c1189b2490e86c714bf64f10f68a3ca21
Mary Green <cyberangel9911@gmail.com>
2020-11-30 17:09:05 -0600

    Hero
    added columns to hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 export default function Hero() {
     return (
-        <div id="hero">
-        </div>
+        <Container fluid id="hero">
+            <Row>
+                <Col className="column_1 hero_col"></Col>
+                <Col className="column_2 hero_col"></Col>
+            </Row>
+        </Container>
+        
 
     )
 }
