4efc72d36d762ac0a1a22ba5356ba40cb44267e5
Mary Green <cyberangel9911@gmail.com>
2020-12-04 15:07:00 -0600

    added logo
    logo added to hero image

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
+
 export default function Hero() {
     return (
         <Container fluid id="hero">
             <Row>
-                <Col className="column_1 hero_col"></Col>
+                <Col className="column_1 hero_col"><img className="aroma-logo" src='/images/aroma-logo.png' /></Col>
                 <Col className="column_2 hero_col"></Col>
             </Row>
         </Container>
