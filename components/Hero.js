4eea49c1d004ea8828501ccef1ae2e5bca7c131a
Mary Green <cyberangel9911@gmail.com>
2020-12-15 20:38:08 -0600

    added hero images

--- components/Hero.js
+++ components/Hero.js
@@ -6,8 +6,8 @@ export default function Hero() {
     return (
         <Container fluid id="hero">
             <Row>
-                <Col className="column_1 hero_col"><img className="aroma-logo" src='/images/aroma-logo.png' /></Col>
-                <Col className="column_2 hero_col">
+                <Col className="column_1 hero_col" md={6}><img className="aroma-logo" src='/images/aroma-logo.png' /></Col>
+                <Col className="column_2 hero_col" md={6}>
                     <HeroSlider />
                 </Col>
             </Row>
--- components/HeroSlider.js
+++ components/HeroSlider.js
@@ -4,24 +4,27 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function HeroSlider() {
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
-    return (
-      <Slider {...settings}>
-        <div>
-          <img src="/images/aroma-hero-1.png" />
-        </div>
-        <div>
-        <img src="/images/aroma-hero-2.png" />
-        </div>
-        <div>
-        <img src="/images/aroma-hero-3.png" />
-        </div>
-      </Slider>
-    );
-  }
+  var settings = {
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <img src="/images/aroma-hero-1.png" className="hero-image" />
+      </div>
+      <div>
+        <img src="/images/aroma-hero-2.png" className="hero-image" />
+      </div>
+      <div>
+        <img src="/images/aroma-hero-3.png" className="hero-image" />
+      </div>
+    </Slider>
+  );
+}
