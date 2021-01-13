af0f618e25b5198e9daba02dbaf28bac8f827bfe
Mary Green <cyberangel9911@gmail.com>
2021-01-12 20:59:09 -0600

    added navbar section

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import HeroSlider from "./HeroSlider";
-
+import Navigation from "./Navigation";
 export default function Hero() {
     return (
         <Container fluid id="hero">
             <Row>
-                <Col className="column_1 hero_col" md={6}><img className="aroma-logo" src='/images/aroma-logo.png' /></Col>
+                <Col className="column_1 hero_col" md={6}><Navigation /><img className="aroma-logo" src='/images/aroma-logo.png' /></Col>
                 <Col className="column_2 hero_col" md={6}>
                     <HeroSlider />
                 </Col>
