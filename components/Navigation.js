693f995a66c43f65467ccaf96aa1f8303abf318d
Mary Green <cyberangel9911@gmail.com>
2021-01-15 21:56:48 -0600

    added Nav links

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Navigation = () => {
     return (
         <div>
-            <h1>nav bar</h1>
+            <nav className="navbar navbar-expand-md navbar-light bg-light">
+                <div>
+                    <ul className= "nav justify-content-center">
+                        <li><a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a></li>
+                        <li><a className="nav-item nav-link" href="#">Menu</a></li> 
+                        <li><a className="nav-item nav-link" href="#">About Us</a></li>
+                        <li><a className="nav-item nav-link" href="#">Contact us</a></li>
+                        <li><a className="nav-item nav-link" href="#">Location</a></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
