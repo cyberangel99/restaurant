85bcdc148a0bd9e5c0ecccce7e11f25dd941cd67
Mary Green <cyberangel9911@gmail.com>
2021-01-17 19:55:53 -0600

    updated navbar
    added color

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { NavDropdown } from 'react-bootstrap';
 
 const Navigation = () => {
     return (
         <div>
-            <nav className="navbar navbar-expand-md navbar-light bg-light">
-                <div>
-                    <ul className= "nav justify-content-center">
-                        <li><a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a></li>
-                        <li><a className="nav-item nav-link" href="#">Menu</a></li> 
-                        <li><a className="nav-item nav-link" href="#">About Us</a></li>
-                        <li><a className="nav-item nav-link" href="#">Contact us</a></li>
-                        <li><a className="nav-item nav-link" href="#">Location</a></li>
-                    </ul>
-                </div>
+            <nav id="navbar">
+                <ul className="nav justify-content-center">
+                    <li><a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a></li>
+                    <li><a className="nav-item nav-link" href="#">Menu</a></li>
+                    <li><a className="nav-item nav-link" href="#">About Us</a></li>
+                    <li><a className="nav-item nav-link" href="#">Contact us</a></li>
+                    <li><a className="nav-item nav-link" href="#">Location</a></li>
+                </ul>
             </nav>
         </div>
     )
